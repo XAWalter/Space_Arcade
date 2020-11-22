64c1ae12bea3f5e62d0ae9b56fcb87e712bc500c
XAWalter <xwalt001@ucr.edu>
2020-11-21 16:49:00 -0800

    Added uart_rx for bt

---
+++ code/bt_control/submodules/uart_rx/testbench/uart_rx_tb.v
@@ -0,0 +1,14 @@
+`timescale 1ns/1ps
+module uart_rx_tb;
+
+
+
+
+
+
+	initial begin
+		$dumpfile(`DUMP_FILE_NAME);
+		$dumpvars(0, uart_rx_tb);
+	end
+endmodule
+
---
+++ code/bt_control/testbench/bt_control_tb.v
@@ -0,0 +1,14 @@
+`timescale 1ns/1ps
+module bt_control_tb;
+
+
+
+
+
+
+	initial begin
+		$dumpfile(`DUMP_FILE_NAME);
+		$dumpvars(0, bt_control_tb);
+	end
+endmodule
+
